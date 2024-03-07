7397d1b71b40b7173ecf072052acd7ef04b9f8be
akshit tyagi <127079896+tyagiakki@users.noreply.github.com>
2024-03-07 12:58:41 +0530

    Add files via upload

---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="Nav">
+        <h1 className="Navv">Top Courses</h1>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
